9e557af7af86cf840f7680e3afd0bd552454ab35
Yusuf Qisthi <yqisthi2002@gmail.com>
2023-02-11 14:02:32 +0700

    change manifest sequence

--- dev_crm_haus/__manifest__.py
+++ dev_crm_haus/__manifest__.py
@@ -2,7 +2,7 @@
     'name' : 'Haus CRM',
     'version' : '1.0',
     'summary':'Haus CRM',
-    'sequence':-999,
+    'sequence':-9,
     'description':"""Haus Customer Relationship Management""",
     'category':'Productivity',
     'website':'',
