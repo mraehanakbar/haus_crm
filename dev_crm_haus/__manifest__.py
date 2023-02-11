fc25a298b79f1fbd196f49712c44c4771a8c4f78
RazoelZ <danydarmawannn@gmail.com>
2023-02-12 04:25:19 +0700

    completing all features except exporting to excel

--- dev_crm_haus/__manifest__.py
+++ dev_crm_haus/__manifest__.py
@@ -1,28 +1,28 @@
 {
-    'name' : 'Haus CRM',
-    'version' : '1.0',
-    'summary':'Haus CRM',
-    'sequence':-9,
-    'description':"""Haus Customer Relationship Management""",
-    'category':'Productivity',
-    'website':'',
+    'name': 'Haus CRM',
+    'version': '1.0',
+    'summary': 'Haus CRM',
+    'sequence': -9,
+    'description': """Haus Customer Relationship Management""",
+    'category': 'Productivity',
+    'website': '',
     'license': 'LGPL-3',
-    'depends':[
+    'depends': [
         'sale',
         'mail',
         'website_slides',
-        'board'
+        'board',
 
     ],
-    'data':[
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
         'views/category.xml',
         'views/issue.xml',
-        'views/menu.xml',
-        'security/ir.model.access.csv',
     ],
-    'demo':[],
-    'qweb':[],
-    'installable':True,
-    'application':True,
-    'auto_install':False,
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
