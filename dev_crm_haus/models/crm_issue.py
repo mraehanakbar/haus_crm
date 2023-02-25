3c8fd4dc0ed1f3118d2dd059b82263022655b72f
Yusuf Qisthi <yqisthi2002@gmail.com>
2023-02-25 20:23:33 +0700

    add departement issue selection

--- dev_crm_haus/models/crm_issue.py
+++ dev_crm_haus/models/crm_issue.py
@@ -2,27 +2,48 @@ from email.policy import default
 from odoo import api, fields, models, _
 from datetime import datetime
 
-
+department_list = [
+        ('OPERATION','Operation'),
+        ('HUMAN RESOURCES','Human Resources'),
+        ('PROCUREMENT & SUPPLY CHAIN','Procurement & Supply Chain'),
+        ('FINANCE & ACCOUNTING','Finance & Accounting'),
+        ('MARKETING','Marketing'),
+        ('INTERNAL AUDIT','Internal Audit'),
+        ('GENERAL MANAGEMENT','General Management'),
+        ('BUSINESS DEVELOPMENT','Bussiness Development'),
+        ('TECHNOLOGY','Technology'),
+    ]
 class CrmIssue(models.Model):
     _name = "crm.issue"
     _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = "CRM Issue Form"
 
     # Define Some Fields Or Function Here
-    employee_id = fields.Many2one(
-        "employee.data", String="Employee", required=True)
+
 
     issue_problem = fields.Char(String="Problem", required=True)
     issue_category = fields.Many2one(
         "crm.category", String="Category", required=True)
-    issue_departement = fields.Selection(
-        String="Departemen", related='employee_id.organization_employee')
+
     issue_due_date = fields.Datetime(String="Due Date")
     issue_comment = fields.Text(String="Comment")
     issue_attachment = fields.Binary("Attachment", attachment=True)
     
-    
+    employee_id = fields.Many2one(
+    "employee.data", String="Employee", required=True)
+    department = fields.Selection(String="Departemen", related='employee_id.organization_employee')
 
+    issue_department = fields.Selection(department_list)
+
+
+# Random function
+    # @api.multi
+    # def get_dep_rec(self):
+    #     sql = "SELECT DISTINCT organization_employee FROM employee.data"
+    #     self.env.cr.execute(sql)
+    #     temp = self.env.cr
+    #     for rec in self.env.cr.fetchall():
+    #         return {'domain': {'employee_id.organization_employee'}}
 #    _defaults = {
 #       'status_by': lambda self, cr, uid, context:
 #   self.pool.get('res.users').browse(cr, uid, uid, context=context).name,
