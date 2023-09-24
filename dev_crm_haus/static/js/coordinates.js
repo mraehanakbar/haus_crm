5bd8079c00f66c4ac91c2e2f5b8d6793a11575c7
Yusuf Qisthi <yqisthi2002@gmail.com>
2023-09-24 19:54:56 +0700

    fix isnear function

--- dev_crm_haus/static/js/coordinates.js
+++ dev_crm_haus/static/js/coordinates.js
@@ -21,43 +21,62 @@ odoo.define("dev_crm_haus.coordinates", function (require) {
         console.log(recordID);
         // If record obtained, browser ask user for coordinates using GPS
         if (recordID) {
-          navigator.geolocation.getCurrentPosition(function (position) {
-            const { latitude, longitude } = position.coords;
-            const coords = {
-              latitude,
-              longitude,
-            };
-            console.log(coords);
-            // Write record
-            rpc
-              .query({
-                model: "crm.issue",
-                method: "write",
-                args: [
-                  [recordID],
-                  { latitude: coords.latitude, longitude: coords.longitude },
-                ],
-              })
-              .then(function () {
-                self.trigger_up("reload");
-                rpc
-                  .query({
-                    model: "crm.issue",
-                    method: "read",
-                    args: [[recordID], ["is_near_one_km"]],
-                  })
-                  .then(function (result) {
-                    if (result[0].is_near_one_km == false) {
-                      self.displayNotification({
-                        type: "warning",
-                        title: "WARNING",
-                        message: "Your Location is too far from office",
-                        sticky: true,
-                      });
-                    }
-                  });
-              });
-          });
+          navigator.geolocation.getCurrentPosition(
+            function (position) {
+              const { latitude, longitude } = position.coords;
+              const coords = {
+                latitude,
+                longitude,
+              };
+              console.log(coords);
+              // Write record
+              rpc
+                .query({
+                  model: "crm.issue",
+                  method: "write",
+                  args: [
+                    [recordID],
+                    { latitude: coords.latitude, longitude: coords.longitude },
+                  ],
+                })
+                .then(function () {
+                  self.trigger_up("reload");
+                  rpc
+                    .query({
+                      model: "crm.issue",
+                      method: "read",
+                      args: [[recordID], ["is_near_one_km"]],
+                    })
+                    .then(function (result) {
+                      if (result[0].is_near_one_km > 1) {
+                        self.displayNotification({
+                          type: "warning",
+                          title: "WARNING",
+                          message: "Your Location is too far from office",
+                          sticky: true,
+                        });
+                      }
+                    });
+                });
+            },
+            function (position) {
+              const { latitude, longitude } = position.coords;
+              const coords = {
+                latitude,
+                longitude,
+              };
+              // Use the coordinates
+            },
+            function (error) {
+              if (error.code === error.PERMISSION_DENIED) {
+                // The user denied location access. Show a message or UI element explaining why it's needed.
+                console.error("Please Allow Location Access");
+              } else {
+                // Handle other errors
+                console.error("Error getting location:", error.message);
+              }
+            }
+          );
         }
         return sup;
       }
